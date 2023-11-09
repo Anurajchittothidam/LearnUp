19662203190cff8ec322ce294d59ad0c57859e66
Anurajchittothidam <anurajchittothidam@gmail.com>
2023-11-09 09:08:53 +0530

    cors changed

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const server=app.listen(port,()=>{
     console.log('port connected to 8000')
 })
 app.use(cors({
-    origin : ['http://localhost:5173/','https://learn-with-learnup.netlify.app','http://learn-with-learnup.netlify.app'],
+    origin : ['*'],
     // origin : [process.env.CLIENTSIDE_URL],
     methods : ["GET","POST" , "DELETE" ,"PUT" , "PATCH"] ,
     credentials: true 
