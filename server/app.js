6c2740f4808e8fe489577375e0356c4045226677
Anurajchittothidam <anurajchittothidam@gmail.com>
2023-11-20 16:54:41 +0530

    video upload bug solved

--- server/app.js
+++ server/app.js
@@ -23,8 +23,8 @@ app.use(cors({
     credentials: true 
 }))
 app.use(express.static(path.resolve()+'/public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true,parameterLimit:100000,limit:'100mb'}))
+app.use(bodyParser.json({limit:"50mb"}))
+app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}))
 
 
 const errorHandler = (err, req, res, next) => {
